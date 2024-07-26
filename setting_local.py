4800552c25bf2b8d230c710c4db35a63c79b9fb4
f.izzo <f.izzo1@studenti.unibg.it>
2024-07-26 16:20:24 +0200

    commit vari

--- setting_local.py
+++ setting_local.py
@@ -1,4 +1,10 @@
-try:
-    from .settings_local import *
-except ImportError:
-    pass
+import os
+from web_project.settings import BASE_DIR
+
+
+STATIC_URL = '/static/'
+
+# Questo dovrebbe già essere presente se stai usando il comando collectstatic
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
